6f717a70b4828fddda9ff8fe300ee78ddd1c975a
dhartley <dani_hartley@msn.com>
2021-06-26 11:38:15 -0500

    added sessions add-on

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 // const homeRoutes = require('./home-routes');
 // const dashboardRoutes = require('./dashboard-routes');
-console.log('hello');
 
-console.log(apiRoutes);
 router.use('/api', apiRoutes);
 // router.use('/', homeRoutes);
 // router.use('/dashboard', dashboardRoutes);
-console.log('goodbye');
 
 router.use((req, res) => {
     res.status(404).end();
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postData = [
     {
         title: 'Hello World',
-        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         user_id: 1
     },
     {
         title: 'Humans Best Friend',
-        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         user_id: 2
     },
     {
         title: 'Pokemon are making a come back',
-        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         user_id: 3
     },
 ];
