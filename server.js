a3281d2e46592394eb2af89a31cbf0d1851058de
dhartley <dani_hartley@msn.com>
2021-06-27 15:17:59 -0500

    added main handlebars

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {}, resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {}, resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
+
+// const helpers =require('./utils/helpers');
+
+//hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('viewengine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(require('./controllers'));
