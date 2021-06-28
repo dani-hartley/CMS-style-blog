715a1bf1802c93af0391dc988743f969b2695f44
dhartley <dani_hartley@msn.com>
2021-06-27 19:52:46 -0500

    update home-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,24 +13,24 @@ router.get('/', (req, res) => {
 });
 
 // Get comment by id
-router.get('/:id', (req, res) => {
-    Comment.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(dbCommentData => {
-            if (!dbCommentData) {
-                res.status(404).json({ message: 'No comment found with this id!' });
-                return;
-            }
-            res.json(dbCommentData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+// router.get('/:id', (req, res) => {
+//     Comment.findOne({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then(dbCommentData => {
+//             if (!dbCommentData) {
+//                 res.status(404).json({ message: 'No comment found with this id!' });
+//                 return;
+//             }
+//             res.json(dbCommentData);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
 
 //Create comment
 router.post('/', withAuth, (req, res) => {
@@ -47,31 +47,31 @@ router.post('/', withAuth, (req, res) => {
 });
 
 //Update comment
-router.put('/:id', withAuth, (req, res) => {
-    Comment.update(
-        {
-            comment_text: req.body.comment_text,
-            user_id: req.session.user_id,
-            post_id: req.body.post_id
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-        .then(dbCommentData => {
-            if (!dbCommentData) {
-                res.status(404).json({ message: 'No comment found with this id!' });
-                return;
-            }
-            res.json(dbCommentData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+// router.put('/:id', withAuth, (req, res) => {
+//     Comment.update(
+//         {
+//             comment_text: req.body.comment_text,
+//             user_id: req.session.user_id,
+//             post_id: req.body.post_id
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         }
+//     )
+//         .then(dbCommentData => {
+//             if (!dbCommentData) {
+//                 res.status(404).json({ message: 'No comment found with this id!' });
+//                 return;
+//             }
+//             res.json(dbCommentData);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
 
 //Delete comment
 router.delete('/:id', withAuth, (req, res) => {
